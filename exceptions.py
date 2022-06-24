9ad8a487e790798a3b98a61f98b195d3f53deaaa
vallenov <a2lenov@mail.ru>
2022-06-24 21:39:17 +0300

    Добавлено новое исключение

--- exceptions.py
+++ exceptions.py
@@ -6,3 +6,9 @@ class FileDBNotFound(TBotExeption):
     def __init__(self, file_name):
         self.file_name = file_name
         super().__init__(f'File {file_name} not found')
+
+
+class ConfigAttributeNotFound(TBotExeption):
+    def __init__(self, attr_name):
+        self.attr_name = attr_name
+        super().__init__(f'Attribute {attr_name} not found')
