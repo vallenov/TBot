6fff2fb5cface389af90b692bccd3dee2facb2cb
vallenov <a2lenov@mail.ru>
2022-06-24 22:29:23 +0300

    Добавлены исключения

--- exceptions.py
+++ exceptions.py
@@ -1,14 +1,31 @@
-class TBotExeption(Exception):
+class TBotException(Exception):
     ...
 
 
-class FileDBNotFound(TBotExeption):
+class FileDBNotFoundError(TBotException):
     def __init__(self, file_name):
         self.file_name = file_name
-        super().__init__(f'File {file_name} not found')
+        super().__init__(file_name)
 
 
-class ConfigAttributeNotFound(TBotExeption):
+class ConfigAttributeNotFoundError(TBotException):
     def __init__(self, attr_name):
         self.attr_name = attr_name
-        super().__init__(f'Attribute {attr_name} not found')
+        super().__init__(attr_name)
+
+
+class EmptySoupDataError(TBotException):
+    def __init__(self, url):
+        self.url = url
+        super().__init__(url)
+
+
+class BadResponseStatusError(TBotException):
+    def __init__(self, status_code):
+        self.status_code = status_code
+        super().__init__(status_code)
+
+
+class UnknownError(TBotException):
+    def __init__(self):
+        super().__init__(f'Unknown error')
