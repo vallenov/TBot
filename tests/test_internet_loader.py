d48d4a54cbaec9dc0987025af95eea55da41347b
vallenov <a2lenov@mail.ru>
2022-07-12 23:12:53 +0300

    Добавлены тесты

---
+++ tests/test_internet_loader.py
@@ -0,0 +1,20 @@
+import re
+
+from TBot.loaders.internet_loader import InternetLoader
+
+
+def test_ip():
+    il = InternetLoader('ILoader')
+    res = il.get_server_ip(privileges=50)
+    ip = res.get('text', False)
+    assert ip, 'No response data'
+    fnd = re.search(r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', ip)
+    if fnd:
+        fnd = fnd.group(0)
+        assert fnd
+    else:
+        assert False, f"Not ip in response ({res.get('text', '')})"
+
+
+if __name__ == '__main__':
+    test_ip()
