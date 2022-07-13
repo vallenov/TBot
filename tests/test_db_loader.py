71453cbbd2c4c7be419b8023a2208f94fd7c32e7
vallenov <a2lenov@mail.ru>
2022-07-13 23:30:49 +0300

    Добавлены тесты для db_loader'а

--- tests/test_db_loader.py
+++ tests/test_db_loader.py
@@ -1,1 +1,20 @@
-import pytest
+from sqlalchemy import exc
+
+from loaders.loader import Loader
+from loaders.db_loader import DBLoader
+from extentions import db
+
+
+def test_db_connection():
+    try:
+        db.session.execute('select 1')
+    except exc.DatabaseError:
+        assert False, 'Database error'
+
+
+def test_get_users():
+    dl = DBLoader('DBLoader')
+    dl.get_users_fom_db()
+    assert type(Loader.users) == dict, 'Wrong return type'
+    assert len(Loader.users) > 0, 'Error get users from DB'
+
