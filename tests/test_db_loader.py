b65b0638b0d02abbd2d8dff003e844c6a02f7e44
vallenov <a2lenov@mail.ru>
2022-08-06 00:55:49 +0300

    Раскомент

--- tests/test_db_loader.py
+++ tests/test_db_loader.py
@@ -31,9 +31,9 @@ def test_get_users():
     assert len(Loader.users) > 0, 'Error get users from DB'
 
 
-# def test_add_user():
-#     dl = DBLoader('DBLoader')
-#     dl.add_user('12345678', 30, 'test_add', 'test_add')
+def test_add_user():
+    dl = DBLoader('DBLoader')
+    dl.add_user('12345678', 30, 'test_add', 'test_add')
 
 
 def test_replace():
