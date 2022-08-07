60c5b9bc8fc542194fa6afdaf5e47cc5c53c6b91
vallenov <a2lenov@mail.ru>
2022-08-07 22:34:08 +0300

    Убран коммент

--- TBot.py
+++ TBot.py
@@ -296,7 +296,6 @@ class TBot:
             'restart_bot': TBot.internet_loader.tbot_restart,
             'restart_system': TBot.internet_loader.system_restart,
             'systemctl': TBot.internet_loader.systemctl,
-            # 'default': TBot.file_loader.get_hello,
             'allow_connection': TBot.internet_loader.allow_connection
         }
         if config.PROD:
