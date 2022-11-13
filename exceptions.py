eeb90bd1fa041dbbf5d6107bc75422f5be2a6ee0
vallenov <a2lenov@mail.ru>
2022-11-14 00:55:33 +0300

    Переработаны исключения

--- exceptions.py
+++ exceptions.py
@@ -1,5 +1,38 @@
+from send_service import send_dev_message
+
+
+RESPONSE_CODES_MAPPING = {
+    0: 'SUCCESS',
+    1: 'INTERNET_ERROR',
+    2: 'FILE_ERROR',
+    3: 'DB_ERROR',
+    4: 'CONFIG_ERROR',
+    5: 'HTTP_ERROR',
+    6: 'PARAMETERS_ERROR',
+    7: 'CACHE_ERROR',
+    100: 'UNKNOWN_ERROR'
+}
+
+
 class TBotException(Exception):
-    ...
+    CODES = RESPONSE_CODES_MAPPING
+
+    def __init__(self, *args, **kwargs):
+        self.context = {
+            'error_type': self.CODES[kwargs.get('code', 100)],
+        }
+        for key, value in kwargs.items():
+            self.context[key] = value
+        super().__init__(*args)
+
+    def send_error(self, trace):
+        if self.context.get('send', False) is True:
+            send_dev_message(
+                data=dict(
+                    subject=self.context['error_type'],
+                    text=f"Message: {self.context['message']}\nTraceback: {trace}"
+                )
+            )
 
 
 class FileDBNotFoundError(TBotException):
