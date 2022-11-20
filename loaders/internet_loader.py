8df84aeb67e78b3eb6f7c83a79f960660a996ee5
vallenov <a2lenov@mail.ru>
2022-11-21 00:34:41 +0300

    Небольшие доработки исключений

--- loaders/internet_loader.py
+++ loaders/internet_loader.py
@@ -654,7 +654,7 @@ class InternetLoader(Loader):
             command = text.split()
             valid_actions = ['start', 'stop', 'restart', 'tunnels']
             if len(command) > 2:
-                raise TBotException(code=6, return_message=f'Wrong parameter count: {len(command)}')
+                raise TBotException(code=6, return_message=f'Wrong parameters count: {len(command)}')
             if len(command) == 1:
                 resp['text'] = 'Выберите действие'
                 resp['markup'] = custom_markup('ngrok',
@@ -698,7 +698,7 @@ class InternetLoader(Loader):
             command = text.split(' ')
             valid_actions = ['start', 'stop', 'restart']
             if len(command) > 2:
-                raise TBotException(code=6, return_message=f'Wrong parameter count: {len(command)}')
+                raise TBotException(code=6, return_message=f'Wrong parameters count: {len(command)}')
             if len(command) == 1:
                 resp['text'] = 'Выберите действие'
                 resp['markup'] = custom_markup('ngrok_db',
@@ -753,7 +753,7 @@ class InternetLoader(Loader):
                 else:
                     raise TBotException(code=6, return_message=f'Wrong parameter value: {cmd[1].lower()}')
             else:
-                raise TBotException(code=6, return_message=f'Wrong parameter count: {len(cmd)}')
+                raise TBotException(code=6, return_message=f'Wrong parameters count: {len(cmd)}')
         except TBotException as e:
             logger.exception(e.context)
             e.send_error(traceback.format_exc())
@@ -770,7 +770,7 @@ class InternetLoader(Loader):
             url = check_config_attribute('system-monitor')
             cmd = text.split()
             if len(cmd) != 3:
-                raise TBotException(code=6, return_message=f'Wrong parameter count: {len(cmd)}')
+                raise TBotException(code=6, return_message=f'Wrong parameters count: {len(cmd)}')
             action = cmd[1].lower()
             service = cmd[2].lower()
             if action not in config.Systemctl.VALID_ACTIONS or service not in config.Systemctl.VALID_SERVICES:
