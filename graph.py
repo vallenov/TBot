002058a226016b510fb9ce7242705e47d59dbb80
vallenov <a2lenov@mail.ru>
2022-11-23 00:25:16 +0300

    Добавлена функция формирования графа для погоды

--- graph.py
+++ graph.py
@@ -1,7 +1,7 @@
 import os
-import datetime
 
 from matplotlib import pyplot as plt
+from helpers import unique_name_from_time
 
 
 class Graph:
@@ -16,12 +16,25 @@ class Graph:
         """
         if not os.path.exists('tmp'):
             os.mkdir('tmp')
-        unique_name = str(datetime.datetime.now()).replace(':', '').replace(' ', '')[:16]
-        img_path = os.path.join('tmp', f'graph_{unique_name}.png')
+        img_path = os.path.join('tmp', f'graph_{unique_name_from_time()}.png')
         plt.figure(figsize=(15, 5))
         plt.plot(dates, count_of_queries)
         plt.xlabel('Date', fontsize=14)
         plt.ylabel('Count of requests', fontsize=14)
+        plt.grid()
+        plt.savefig(img_path)
+        plt.close()
+        return img_path
+
+    @staticmethod
+    def get_weather_graph(dates: list, tepmerature: list):
+        if not os.path.exists('tmp'):
+            os.mkdir('tmp')
+        img_path = os.path.join('tmp', f'weather_{unique_name_from_time()}.png')
+        plt.figure(figsize=(15, 5))
+        plt.plot(dates, tepmerature)
+        plt.xlabel('Date', fontsize=14)
+        plt.ylabel('Temperature (°C)', fontsize=14)
         plt.grid()
         plt.savefig(img_path)
         plt.close()
