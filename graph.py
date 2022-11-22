ce4ef55860d342845e88b5d5dd53701319d2d0b5
vallenov <a2lenov@mail.ru>
2022-11-23 00:48:41 +0300

    Разработан более универсальный метод генерации графиков

--- graph.py
+++ graph.py
@@ -1,40 +1,23 @@
 import os
+from collections import namedtuple
 
 from matplotlib import pyplot as plt
 from helpers import now_time
+
+BaseGraphInfo = namedtuple('BaseGraphInfo', ['type', 'xname', 'yname', 'x', 'y'])
 
 
 class Graph:
 
     @staticmethod
-    def get_statistic_graph(dates: list, count_of_queries: list) -> str:
-        """
-        Create graph image
-        :param dates: list of dates
-        :param count_of_queries: list of counts
-        :return: path to image
-        """
+    def get_base_graph(base: BaseGraphInfo):
         if not os.path.exists('tmp'):
             os.mkdir('tmp')
-        img_path = os.path.join('tmp', f'graph_{now_time()}.png')
+        img_path = os.path.join('tmp', f'{base.type}_{now_time()}.png')
         plt.figure(figsize=(15, 5))
-        plt.plot(dates, count_of_queries)
-        plt.xlabel('Date', fontsize=14)
-        plt.ylabel('Count of requests', fontsize=14)
-        plt.grid()
-        plt.savefig(img_path)
-        plt.close()
-        return img_path
-
-    @staticmethod
-    def get_weather_graph(dates: list, temperature: list):
-        if not os.path.exists('tmp'):
-            os.mkdir('tmp')
-        img_path = os.path.join('tmp', f'weather_{now_time()}.png')
-        plt.figure(figsize=(15, 5))
-        plt.plot(dates, temperature)
-        plt.xlabel('Date', fontsize=14)
-        plt.ylabel('Temperature (°C)', fontsize=14)
+        plt.plot(base.x, base.y)
+        plt.xlabel(base.xname, fontsize=14)
+        plt.ylabel(base.yname, fontsize=14)
         plt.grid()
         plt.savefig(img_path)
         plt.close()
