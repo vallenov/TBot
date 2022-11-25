d0d920f2892d5b2a9dc948fee4dd00e0cbcfc233
vallenov <a2lenov@mail.ru>
2022-11-25 18:19:04 +0300

    Добавлены отдельные виды графиков

--- loaders/db_loader.py
+++ loaders/db_loader.py
@@ -15,7 +15,7 @@ from markup import custom_markup
 from send_service import send_dev_message
 from loggers import get_logger
 from exceptions import TBotException
-from graph import Graph, BaseGraphInfo
+from graph import Graph, BaseGraphInfo, BaseSubGraphInfo
 
 logger = get_logger(__name__)
 
@@ -468,7 +468,7 @@ class DBLoader(Loader):
                     for cur in plot_data:
                         dt.append(cur[0])
                         cnt.append(cur[1])
-                    bgi = BaseGraphInfo('Statistic', 'statistic', 'Date', 'Count', dt, cnt)
+                    bgi = BaseGraphInfo('Statistic', 'statistic', [BaseSubGraphInfo('plot', 'Date', 'Count', dt, cnt)])
                     resp['photo'] = Graph.get_base_graph(bgi)
                 to_sort = md.LogRequests.query \
                     .join(md.Users, md.LogRequests.chat_id == md.Users.chat_id) \
