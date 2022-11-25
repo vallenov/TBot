aeaf837345ba74615b233c75ad67a420fc72b67f
vallenov <a2lenov@mail.ru>
2022-11-25 17:57:49 +0300

    Усовершенствован алгоритм генерации графика

--- graph.py
+++ graph.py
@@ -4,7 +4,8 @@ from collections import namedtuple
 from matplotlib import pyplot as plt
 from helpers import now_time
 
-BaseGraphInfo = namedtuple('BaseGraphInfo', ['title', 'type', 'xname', 'yname', 'x', 'y'])
+BaseGraphInfo = namedtuple('BaseGraphInfo', ['title', 'type', 'subplots'])
+BaseSubGraphInfo = namedtuple('BaseGraphInfo', ['xname', 'yname', 'x', 'y'])
 
 
 class Graph:
@@ -14,12 +15,14 @@ class Graph:
         if not os.path.exists('tmp'):
             os.mkdir('tmp')
         img_path = os.path.join('tmp', f'{base.type}_{now_time()}.png')
-        plt.figure(figsize=(15, 5))
-        plt.plot(base.x, base.y)
-        plt.title(base.title or '')
-        plt.xlabel(base.xname, fontsize=14)
-        plt.ylabel(base.yname, fontsize=14)
-        plt.grid()
+        plt.title(base.title)
+        plt.figure(figsize=(15, 5 * len(base.subplots)))
+        for i, splot in enumerate(base.subplots):
+            plt.subplot(len(base.subplots), 1, i+1)
+            plt.plot(splot.x, splot.y)
+            plt.xlabel(splot.xname, fontsize=14)
+            plt.ylabel(splot.yname, fontsize=14)
+            plt.grid()
         plt.savefig(img_path)
         plt.close()
         return img_path
