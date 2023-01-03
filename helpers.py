eeeca0ea54fb077eaf19aaeb4d31c027af116fef
vallenov <a2lenov@mail.ru>
2023-01-04 00:41:52 +0300

    Добавлена аннотация типов

--- helpers.py
+++ helpers.py
@@ -82,7 +82,7 @@ def is_phone_number(number: str) -> str or None:
     return raw_num
 
 
-def check_config_attribute(attr: str) -> str or Exception:
+def check_config_attribute(attr: str) -> str:
     if config.LINKS.get(attr, None):
         return config.LINKS[attr]
     else:
