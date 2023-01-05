1e8eefb927195003227c95ca4ae2f7425e249dfd
vallenov <a2lenov@mail.ru>
2023-01-06 00:42:18 +0300

    Добавлена аннотация типов

--- exceptions.py
+++ exceptions.py
@@ -43,5 +43,5 @@ class TBotException(Exception):
                 )
             )
 
-    def return_message(self):
+    def return_message(self) -> dict:
         return dict(text=self.context.get('return_message', 'Something wrong'))
