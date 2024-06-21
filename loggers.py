8ba8dbf3007ff0039634784d4c81040e44778cbf
vallenov <a2lenov@mail.ru>
2024-06-21 23:06:24 +0300

    add init dirs

--- loggers.py
+++ loggers.py
@@ -4,6 +4,26 @@ import os
 logging.basicConfig(level=logging.INFO)
 
 
+def init_dirs():
+    """
+    Init downloads dir
+    """
+    curdir = os.curdir
+    if not os.path.exists(os.path.join(curdir, 'downloads')):
+        os.mkdir(os.path.join(curdir, 'downloads'))
+        os.chown(os.path.join(curdir, 'downloads'), 1000, 1000)
+    if not os.path.exists(os.path.join('downloads', 'text')):
+        os.mkdir(os.path.join('downloads', 'text'))
+        os.chown(os.path.join('downloads', 'text'), 1000, 1000)
+    print(os.path.join(curdir, 'tests', 'downloads'))
+    if not os.path.exists(os.path.join(curdir, 'tests', 'downloads')):
+        os.mkdir(os.path.join(curdir, 'tests', 'downloads'))
+        os.chown(os.path.join(curdir, 'tests', 'downloads'), 1000, 1000)
+    if not os.path.exists(os.path.join('tests', 'downloads', 'text')):
+        os.mkdir(os.path.join('tests', 'downloads', 'text'))
+        os.chown(os.path.join('tests', 'downloads', 'text'), 1000, 1000)
+
+
 def get_logger(name: str):
     logger = logging.getLogger(name)
     handler = logging.FileHandler('run.log')
@@ -14,6 +34,7 @@ def get_logger(name: str):
 
 
 def get_conversation_logger():
+
     conversation_logger = logging.getLogger('conversation')
     conversation_logger.setLevel(logging.INFO)
     conv_handler = logging.FileHandler(os.path.join('downloads', 'text', 'run_conv.log'))
