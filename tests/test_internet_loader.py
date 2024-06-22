b8ffc0d79a7b513db3999768c94d2318df3c8c30
vallenov <a2lenov@mail.ru>
2024-06-22 14:18:02 +0300

    add test_routes and test_get_weather

--- tests/test_internet_loader.py
+++ tests/test_internet_loader.py
@@ -1,9 +1,14 @@
 import re
+import os
 from bs4 import BeautifulSoup
 
 import config
 from loaders.internet_loader import InternetLoader
 from loaders.loader import LoaderRequest
+
+from helpers import (
+    check_config_attribute
+)
 
 
 def test_ip():
@@ -33,6 +38,14 @@ def test_site_to_lxml():
         assert False, f'Wrong response data {res}'
 
 
+def test_routes():
+    """Test all the routes"""
+    il = InternetLoader()
+    url = check_config_attribute('exchange_url')
+    res = il.regular_request(url)
+    assert res.status_code == 200
+
+
 def test_get_exchange():
     il = InternetLoader()
     request = LoaderRequest(text='', privileges=30, chat_id='')
@@ -41,3 +54,13 @@ def test_get_exchange():
     assert search_raw
     currency = search_raw.group(0)
     assert currency in config.EXCHANGES_CURRENCIES
+
+
+def test_get_weather():
+    il = InternetLoader()
+    request = LoaderRequest(text='weather Москва', privileges=30, chat_id='')
+    res = il.get_weather(request)
+    search_raw = re.search(r'weather_.*\.png', res.photo)
+    assert search_raw
+    photo_path = search_raw.group(0)
+    assert os.path.exists(os.path.join('tmp', photo_path))
